f7574ad469dfb95139a1d0915a865a397e4d0aa4
James Grady <jlgrady1@gmail.com>
2016-09-15 14:37:06 -0700

    fix tests

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     test_files = ' '.join(sys.argv[1:])
     init = False
 
-os.environ['PYTHONPATH'] = '.'
+os.environ['PYTHONPATH'] = os.path.dirname(os.path.realpath(__file__))
 
 status = pytest.main('{}'.format(test_files))
 exit(status)
